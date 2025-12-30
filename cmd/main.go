c50f98668cc9fc6c6d2671df36c6b7779300bc58
Jeffrey Omoakah <jomoakah3@gmail.com>
2025-12-30 08:26:21 +0100

    implement full HTTP server with middleware, routing and health end point

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/JeffreyOmoakah/AUTH.git/internal/env"
+	"github.com/jackc/pgx/v5"
+)
+
+
+
+func main() {
+	ctx := context.Background()
+	
+	cfg := config{
+		addr: env.GetString("ADDR", ":3000"),
+		db: dbConfig{
+			dsn:env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=auth sslmode=disable"),
+		},
+	}
+	
+	// Logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+	
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+		if err != nil {
+			slog.Error("failed to connect to database", "error", err)
+			os.Exit(1)
+		}
+		defer conn.Close(ctx)
+		
+		logger.Info("connected to database","dsn",cfg.db.dsn)
+		
+		api := application{
+			config: cfg,
+			db: conn,
+		}
+		
+		api.run(api.mount())
+		
+			if err := api.run(api.mount()); err != nil {
+				slog.Error("server failed to start", "error", err)
+				os.Exit(1)
+			}
+}
